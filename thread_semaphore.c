5d22907639d9426e143292df3c1568c9b35b56cf
Victor Hugo Oliveira Leão <victor.pessoal1203@gmail.com>
2022-07-18 17:52:05 -0300

    Update thread_semaphore.c

--- thread_semaphore.c
+++ thread_semaphore.c
@@ -1,34 +1,73 @@
+// NÃO FUNCIONA 100%!!
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 
-sem_t semaforo;
-int ticket = 1000;
+#define NMAX 1000
+
+sem_t s1;
+int tickets[NMAX];
+int tks_qtd;
 
 // numero indisponivel
 int get_ticket(){
-    sem_wait(&semaforo);
-    return ticket;
+    sem_wait(&s1);
+
+    static int iniciado = 0;
+    
+    if(iniciado==0){
+        iniciado++;
+        for(int i = 0; i<NMAX; i++)
+            tickets[i]=100+i;
+    tks_qtd=0;
+    }
+    
+    int ret;
+    if(tks_qtd == NMAX){
+        ret=-1
+    } else{
+        ret = tickets[tks_qtd++]; 
+        tickets[tks_qtd-1]=-2; // ja foi entregue
+    }
+
+    sem_post(&s1);
+
+    return ret;
 }
 
-// numero fica disponivel quando chama
-void *return_ticket(int i){
-    sem_post(&semaforo);
-    int ret_ticket = get_ticket();
-    printf("Ticket recebido = %d\n", ret_ticket);
+void *comprador(void *arg){
+    int tid= *(int*)arg;
+    int comprados[100], comprados_i=6;
+    // Vai comprar 5 tickets
+    for(int i=0; i<5; i++){
+        int x = get_ticket();
+        comprados[comprados_i--]=x;
+        printf("%d comprou: %d\n", tid, x);
+        sleep(2); // garante que diferentes threads estao trabalhando ao msm tempo -> sem nao tem alternancia
+    }
+    for(int i = 0; i < 5; i++){
+        return_ticket(comprados[i]);
+        printf("%d comprou:: %d\n", tid, comprados[i]);
+    }
 }
 
-// loop com 2 threads:
-// dorme um segundo -> get ticket
-// dorme outro segundo -> return ticket
+void return_ticket(int i){
+    if(tickets[t-100]==-2){
+        tickets[t-100]=t; // ticket ja foi entregue, devolve ele
+    }
+}
 
 int main(void){
-    sem_init(&semaforo, 0, 1);
+    sem_init(&s1, 0, 1);
+    pthread_t t[6];
+    int x[6];
+    for(int i = 1; i<6; i++){
+        x[i]=i;
+        pthread_create(&t[i], NULL, comprador, (void*)&x[i]);
+    }
 
-    pthread_t ret_t;
-
-    pthread_create(&ret_t, NULL, return_ticket, NULL);
-
-    return 0;
+    for(int i = 1; i<6; i++)
+        pthread_join(t[i], NULL);
 }
